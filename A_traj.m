3e10e43e359b7fc8615592e339d3327339dee856
Yan Wang <ywang28@wpi.edu>
2018-05-16 13:23:38 -0400

    1.replace the nonlinear constraint a linear one. 2.replace subs with a function. 3.optimize some code. 4.generate another feasible optimal trajectory.

---
+++ A_traj.m
@@ -0,0 +1,50 @@
+function [A, b] = A_traj(tr)
+
+f_num = tr.n_H*2 + 1;
+l_v = tr.dof*f_num;
+interval = tr.period/tr.num;
+vt = 0:interval:tr.period;
+
+A = [];
+b = [];
+
+for t = vt
+    qi = zeros(3,l_v);
+    dqi = zeros(3,l_v);
+    ddqi = zeros(3,l_v);
+    
+    q_tmp = zeros(1,f_num);
+    q_tmp(1,1) = 1;
+    for i = 1:tr.n_H
+        q_tmp(1,i+1) = 1/(tr.w_f*i)*sin(tr.w_f*i*t);
+        q_tmp(1,i+1+tr.n_H) = -1/(tr.w_f*i)*cos(tr.w_f*i*t);
+    end
+    qi(1,1:f_num) = q_tmp;
+    qi(2,1+f_num:2*f_num) = q_tmp;
+    qi(3,1+2*f_num:3*f_num) = q_tmp;
+
+    q_tmp(1,1) = 0;
+    for i = 1:tr.n_H
+        q_tmp(1,i+1) = cos(tr.w_f*i*t);
+        q_tmp(1,i+1+tr.n_H) = sin(tr.w_f*i*t);
+    end
+    dqi(1,1:f_num) = q_tmp;
+    dqi(2,1+f_num:2*f_num) = q_tmp;
+    dqi(3,1+2*f_num:3*f_num) = q_tmp;
+    
+    q_tmp(1,1) = 1;
+    for i = 1:tr.n_H
+        q_tmp(1,i+1) = -tr.w_f*i*sin(tr.w_f*i*t);
+        q_tmp(1,i+1+tr.n_H) = tr.w_f*i*cos(tr.w_f*i*t);
+    end
+    ddqi(1,1:f_num) = q_tmp;
+    ddqi(2,1+f_num:2*f_num) = q_tmp;
+    ddqi(3,1+2*f_num:3*f_num) = q_tmp;
+    
+    A = [A; qi; dqi; ddqi; -qi; -dqi; -ddqi];
+    b_tmp1 = [tr.max_q1; tr.max_q2; tr.max_q3; tr.max_dq; tr.max_dq; tr.max_dq; tr.max_ddq; tr.max_ddq; tr.max_ddq;];
+    b_tmp2 = [-tr.min_q1; -tr.min_q2; -tr.min_q3; tr.max_dq; tr.max_dq; tr.max_dq; tr.max_ddq; tr.max_ddq; tr.max_ddq;];
+    b = [b; b_tmp1; b_tmp2];
+end
+end
+
--- optimal_exciting_traj.m
+++ optimal_exciting_traj.m
@@ -2,19 +2,29 @@ function [tr] = optimal_exciting_traj(h, n_H, w_f)
 
 tr.n_H = n_H;
 tr.w_f = w_f;
-tr.period = 2*pi/tr.w_f;
+tr.period = 2*3.1415926/tr.w_f;
 tr.num = 2^(n_H+1);
 tr.dof = size(h,1);
+tr.base_num = size(h,2);
 
 tr.max_q = 0.5;
 tr.min_q = -0.5;
+q_delta = 0.25;
+tr.max_q1 = 0.785398 - q_delta;
+tr.min_q1 = -1.309 + q_delta;
+tr.max_q2 = 0.785398 - q_delta;
+tr.min_q2 = -0.785398 + q_delta;
+tr.max_q3 = 0.785398 - q_delta;
+tr.min_q3 = -0.785398 + q_delta;
+
+
 tr.max_dq = 2;
 tr.min_dq = -2;
 tr.max_ddq = 5;
 tr.min_ddq = -5;
 
 q_init_max = 0.1;
-ab_init_max = 0.05;
+ab_init_max = 0.1;
 
 q01 = rand()*q_init_max-q_init_max/2;
 a1 = rand(1,tr.n_H)*ab_init_max-ab_init_max/2;
@@ -28,8 +38,7 @@ b3 = rand(1,tr.n_H)*ab_init_max-ab_init_max/2;
 
 
 x0 = [q01 a1 b1 q02 a2 b2 q03 a3 b3];
-A = [];
-b = [];
+[A, b] = A_traj(tr);
 Aeq = [];
 beq = [];
 ub = ones(1,length(x0));
@@ -37,11 +46,12 @@ lb = -ub;
 
 
 
-objfun = @(x)cond_traj(x, h, tr);
-nonlcon = @(x)nonlcon_traj(x, h, tr);
+objfun = @(x)cond_traj(x, tr);
+nonlcon = [];
+%nonlcon = @(x)nonlcon_traj(x, h, tr);
 % options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
-options = optimoptions(@fmincon,'Display','iter','Algorithm','active-set','MaxFunctionEvaluations',20000, 'MaxIterations', 15);
-[x,fval,exitflag,output,lambda,grad,hessian] = fmincon(objfun,x0,A,b,Aeq,beq,lb,ub,nonlcon,options);
+options = optimoptions(@fmincon,'Display','iter','Algorithm','active-set','MaxFunctionEvaluations',20000, 'MaxIterations', 6);
+[x,fval,exitflag,output,lambda,grad,hessian] = fmincon(objfun,x0,A,b,Aeq,beq,lb,ub, nonlcon, options);
 
 tr.q01 = x(1);
 tr.a1 = x(2:1+tr.n_H);
