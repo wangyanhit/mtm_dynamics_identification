ae50be46895d069adff4ac32bc1e32c7770a9491
Yan Wang <ywang28@wpi.edu>
2018-05-18 12:52:53 -0400

    add all the figures into one

--- plot_excitation_traj.m
+++ plot_excitation_traj.m
@@ -28,6 +28,7 @@ for i = 1:length(vt)
 end
 
 figure
+subplot(3,1,1)
 plot(vt, q1, '-r', 'LineWidth', 2)
 hold on
 plot(vt, q2, '--g', 'LineWidth', 2)
@@ -36,7 +37,7 @@ xlabel('t (s)')
 ylabel('Joint angle (rad)')
 legend('q1', 'q2', 'q3')
 
-figure
+subplot(3,1,2)
 plot(vt, dq1, '-r', 'LineWidth', 2)
 hold on
 plot(vt, dq2, '--g', 'LineWidth', 2)
@@ -45,7 +46,7 @@ xlabel('t (s)')
 ylabel('Joint velocity (rad/s)')
 legend('dq1', 'dq2', 'dq3')
 
-figure
+subplot(3,1,3)
 plot(vt, ddq1, '-r', 'LineWidth', 2)
 hold on
 plot(vt, ddq2, '--g', 'LineWidth', 2)
